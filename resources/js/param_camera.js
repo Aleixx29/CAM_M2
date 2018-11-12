9dfcfaaa7e8b2b74a8dc1a888037deed0c333c8b
Prémel-Cabic Arnaud <arnaud.premel-cabic@isen-bretagne.fr>
2018-11-12 18:10:34 +0100

    Responsible video canvas
    canvas is were we draw the video and the shooting cross is not responsive only  with the initial dimmension of the windows

--- resources/js/param_camera.js
+++ resources/js/param_camera.js
@@ -16,101 +16,118 @@ navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia 
 
 /***************/
 
-var video = document.getElementById("myVideo");
-var image = document.getElementById("myImage");
-var canvas = document.getElementById("myCanvas");
-var videoCanvas = document.getElementById("myVideoCanvas");
+var streaming = false;
+width = document.getElementById("player").offsetWidth;
+console.log(width);
+
+
+video = document.getElementById('video');
+canvas = document.getElementById('canvas');
+photo = document.getElementById('photo');
+startbutton = document.getElementById('picShoot');
+ctx = canvas.getContext('2d');
 
 /******* Listeners pour les boutons ******/
 
 document.getElementById("picSave").addEventListener("click", function () {
-    document.getElementById("downloadImage").href = videoCanvas.toDataURL('image/webp');
-});
-
-document.getElementById("picShoot").addEventListener("click", function () {
-
-    if (myStream) {
-        // "image/webp" works in Chrome 18. In other browsers, this will fall back to image/png.
-        image.src = videoCanvas.toDataURL('image/webp');
-        // Active le bouton save
-        document.getElementById("downloadImage").onclick = "return true;";
-    }
-
-    image.style.display = "block";
-    canvas.style.display = "none";
-
-    var date = new Date();
-    $("#dateTime").empty().append(date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear() + " : " +
-        date.getHours() + "h" + date.getMinutes());
-    if (waitting) {
-        alert("La localisation n'a pas eu le temps de charger");
-    }
-    setCoordonnees();
-
+    document.getElementById("downloadImage").href = photo.src;
 });
 
 document.getElementById("picReset").addEventListener("click", function () {
     // affiche le canvas
     canvas.style.display = "block";
-    image.style.display = "none";
+    photo.style.display = "none";
 
     $("#dateTime").empty();
     $("#latitude").empty();
     $("#longitude").empty();
 });
 
-canvas.width = 640;
-canvas.height = 480;
-videoCanvas.width = 640;
-videoCanvas.height = 480;
+navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    .then(function (stream) {
+        video.srcObject = stream;
+        video.play();
+    })
+    .catch(function (err) {
+        console.log("An error occurred! " + err);
+    });
+video.addEventListener('canplay', function (ev) {
+    if (!streaming) {
+        height = video.videoHeight / (video.videoWidth / width);
 
-var ctxVideoCanvas = videoCanvas.getContext('2d');
-var ctx = canvas.getContext('2d');
+        video.setAttribute('width', width);
+        video.setAttribute('height', height);
+        canvas.setAttribute('width', width);
+        canvas.setAttribute('height', height);
+        photo.style.width = width + 'px';
+        photo.style.height = height + 'px';
 
-var myStream = null;
-
-var xStart = 0;
-var yStart = 0;
-
-var xStop = 480;
-var yStop = 480;
-
-var x = xStart;
-var y = yStart;
-
-function canvasPlayer() {
-    if (myStream) {
-        ctx.drawImage(video, 0, 0);
-        ctxVideoCanvas.drawImage(video, 0, 0);
-        var currentImage = ctx.getImageData(0, 0, canvas.width, canvas.height);
-        var pixels = currentImage.data;
-        ctx.putImageData(currentImage, 0, 0);
-
-        // draw the cross
-        ctx.beginPath();
-        ctx.lineWidth = '3';
-        ctx.strokeStyle = '#4C8';
-        ctx.arc(canvas.width / 2, canvas.height / 2, 50, 0, Math.PI * 2, false);
-        ctx.stroke();
-        ctx.closePath();
+        streaming = true;
     }
-}
-if (navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia({
-        // Constraints
-        audio: true,
-        video: true
-    }).then(
-        // Promise Success
-        function (localMediaStream) {
-            video.srcObject = localMediaStream;
-            myStream = localMediaStream;
-            // call canvasPlayer every 20ms
-            window.setInterval(canvasPlayer, 20);
-        }).catch(
-        // Promise Catch
-        function (err) {
-            console.log('The following error occurred when trying to use getUserMedia: ' + err);
+}, false);
+video.addEventListener('play', function () {
+    var $this = this; //cache
+    (function loop() {
+        if (!$this.paused && !$this.ended) {
+            // draw video
+            // fill horizontally
+            var hRatio = (canvas.width / video.videoWidth) * video.videoHeight;
+            ctx.drawImage($this, 0,0, canvas.width, hRatio);
+            // draw the cross
+            ctx.beginPath();
+            ctx.lineWidth = '3';
+            ctx.strokeStyle = '#4C8';
+            ctx.arc(canvas.width / 2, canvas.height / 2, 50, 0, Math.PI * 2, false);
+            ctx.moveTo(canvas.width / 2, canvas.height / 2 - 55);
+            ctx.lineTo(canvas.width / 2, canvas.height / 2 + 55);
+            ctx.moveTo(canvas.width / 2 - 55, canvas.height / 2);
+            ctx.lineTo(canvas.width / 2 + 55, canvas.height / 2);
+            ctx.stroke();
+            ctx.closePath();
+            setTimeout(loop, 1000 / 30); // drawing at 30fps
         }
-    );
+    })();
+}, 0);
+video.addEventListener('loadedmetadata', function () {
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+});
+startbutton.addEventListener('click', function (ev) {
+    takepicture();
+    ev.preventDefault();
+}, false);
+clearphoto();
+
+function clearphoto() {
+    var context = canvas.getContext('2d');
+    context.fillStyle = "#AAA";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    var data = canvas.toDataURL('image/png');
+    photo.setAttribute('src', data);
+}
+
+function takepicture() {
+    var context = canvas.getContext('2d');
+    if (width && height) {
+
+        canvas.width = width;
+        canvas.height = height;
+        context.drawImage(video, 0, 0, width, height);
+
+        var data = canvas.toDataURL('image/png');
+        photo.setAttribute('src', data);
+        photo.style.display = "block";
+        canvas.style.display = "none";
+        document.getElementById("downloadImage").onclick = "return true;";
+        var date = new Date();
+        $("#dateTime").empty().append(date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear() + " : " +
+            date.getHours() + "h" + date.getMinutes());
+        if (waitting) {
+            alert("La localisation n'a pas eu le temps de charger");
+        }
+        setCoordonnees();
+    } else {
+        clearphoto();
+    }
 }
